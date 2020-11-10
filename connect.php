4d9b713a8732bd4fbee58a9451ce5ee9486a03fc
Jason <cya75@sfu.ca>
2020-11-09 20:42:27 -0800

    Created file to connect to database

---
+++ connect.php
@@ -0,0 +1,18 @@
+<?php
+
+
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "dan_peng";
+
+$db = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+if(mysqli_connect_error()){
+	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+
+
+
+?>
